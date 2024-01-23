7e5a15d8a563c0df352aab28ac27628401f4859e
Nick Trogh <1908215+ntrogh@users.noreply.github.com>
2024-01-23 14:49:10 +0100

    Add comments

--- api/discounts-get/index.js
+++ api/discounts-get/index.js
@@ -1,5 +1,10 @@
 const data = require('../shared/discount-data');
 
+/**
+ * Retrieves discounts and sends them as a JSON response.
+ * @param {Object} context - The Azure Function context object.
+ * @param {Object} req - The HTTP request object.
+ */
 module.exports = async function (context, req) {
     try {
         const discounts = data.getDiscounts();
